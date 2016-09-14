5180a5b4cea0445431311c682917f62ed528d803
Carlos Ginés <efrel.v2@gmail.com>
2016-09-14 22:35:02 +0200

    Added NavMeshObstacles to units to improve navigation.

--- Assets/Scripts/Gameplay/Moving/Movable.cs
+++ Assets/Scripts/Gameplay/Moving/Movable.cs
@@ -12,8 +12,7 @@ namespace EfrelGames
 		#region Public fields and properties
 		//======================================================================
 
-		/// <summary>Current destination for this unit.</summary>
-
+		/// <summary>Current destination for this unit (reactive).</summary>
 		private ReactiveProperty<Destination> _reactiveDest;
 		public Destination Dest { 
 			get { return _reactiveDest.Value; } 
@@ -29,7 +28,9 @@ namespace EfrelGames
 		#region Cached components
 		//======================================================================
 
+		private Transform _trans;
 		private NavMeshAgent _agent;
+		private NavMeshObstacle _obstacle;
 
 		#endregion
 
@@ -41,6 +42,16 @@ namespace EfrelGames
 		/// Stream checking if current destination is reached.
 		/// </summary>
 		private IDisposable _pathCheckStream;
+		/// <summary>
+		/// Observable that checks if we have reached the destination.
+		/// </summary>
+		IObservable<bool> _pathCheckObs;
+		/// <summary>
+		/// Observable that start a path after NavMesh is available and then
+		/// checks if we have reached the destination.
+		/// </summary>
+		IObservable<bool> _delayedStrartPathCheckObs;
+		
 
 		#endregion
 
@@ -51,8 +62,30 @@ namespace EfrelGames
 		void Awake ()
 		{
 			// Cache components
+			_trans = transform;
 			_agent = GetComponent <NavMeshAgent> ();
+			_obstacle = GetComponent <NavMeshObstacle> ();
 			_reactiveDest = new ReactiveProperty<Destination> ();
+
+			// Prepare observables
+			_pathCheckObs = Observable.EveryUpdate ()
+				.TakeUntilDestroy (gameObject)
+				.TakeWhile (_ => Dest != null && !this.DestinationReached ())
+				.ContinueWith (Observable.Return (Dest != null));
+			
+			_delayedStrartPathCheckObs = Observable.EveryUpdate()
+				.Where(_ => {
+					NavMeshHit hit;
+					return NavMesh.SamplePosition(
+							_trans.position, out hit, 0.01f, NavMesh.AllAreas
+					);
+				})
+				.First()
+				.Do (_ => {
+					_agent.enabled = true;
+					_agent.SetDestination (Dest.Position);
+				})
+				.ContinueWith (_pathCheckObs);
 		}
 
 		#endregion
@@ -71,8 +104,7 @@ namespace EfrelGames
 		/// </returns>
 		public IObservable<bool> Add (Destination dest)
 		{
-			if ((Dest == null || (int)Dest.Type <= (int)dest.Type) 
-					&& _agent.enabled) {
+			if ((Dest == null || (int)Dest.Type <= (int)dest.Type)) {
 //				Debug.Log ("Added destination " + dest);
 				Dest = dest;
 				return this.StartPath ();;
@@ -87,7 +119,7 @@ namespace EfrelGames
 		/// <returns>Whether the destionation has been removed.<returns>
 		public bool Remove (Destination dest)
 		{
-			if (Dest != null && Dest == dest && _agent.enabled) {
+			if (Dest != null && Dest == dest) {
 				Dest = null;
 				_agent.ResetPath ();
 				return true;
@@ -103,7 +135,7 @@ namespace EfrelGames
 		/// <returns>Whether the destionation has been removed.<returns>
 		public bool Remove (DestType type)
 		{
-			if (Dest != null && Dest.Type == type && _agent.enabled) {
+			if (Dest != null && Dest.Type == type) {
 				Dest = null;
 				_agent.ResetPath ();
 				return true;
@@ -117,42 +149,6 @@ namespace EfrelGames
 		#region Private methods
 		//======================================================================
 
-		/// <summary>
-		/// Starts a new path towards current destination.
-		/// </summary>
-		/// <returns>
-		/// Observable that emits true if we have reached the destination.
-		/// </returns>
-		private IObservable<bool> StartPath ()
-		{
-			_agent.SetDestination (Dest.Position);
-			_agent.stoppingDistance = Dest.Distance;
-
-			// Observable that checks if we have reached the destination.
-			IObservable<bool> pathCheckObs = Observable.EveryUpdate ()
-				.TakeUntilDestroy (gameObject)
-				.TakeWhile (_ =>
-					Dest != null && !this.DestinationReached ()
-				).ContinueWith (
-					Observable.Return (1).Select (_ => Dest != null)
-				);
-
-			// Reset the path check stream
-			if (_pathCheckStream != null) {
-				_pathCheckStream.Dispose ();
-			}
-			_pathCheckStream = pathCheckObs.Subscribe (reached => {
-				if (reached) {
-					Debug.Log (name + " Destination reached " + Dest);
-					this.Remove (Dest);
-				} else {
-					Debug.Log (name + " Destination removed. ");
-				}
-			});
-
-			return pathCheckObs;
-		}
-
 		/// <summary>Check agent reached the destination.</summary>
 		private bool DestinationReached ()
 		{
@@ -160,6 +156,55 @@ namespace EfrelGames
 				&& !_agent.pathPending
 				&& _agent.remainingDistance <= _agent.stoppingDistance
 				&& (!_agent.hasPath || _agent.velocity.sqrMagnitude == 0f);
+		}
+			
+		/// <summary>
+		/// Starts a new path towards current destination. It deals with a
+		/// combination of NavMeshAgent and NavMeshObstacle. It is required a
+		/// delayed start of the path if obstable is enabled, so that 
+		/// NavMeshAgent is on the NavMesh.
+		/// </summary>
+		/// <returns>
+		/// Observable that emits true if we have reached the destination.
+		/// </returns>
+		private IObservable<bool> StartPath ()
+		{
+			IObservable<bool> pathCheckObs;
+						
+			if (_agent.isOnNavMesh) {
+				// Start path and set regular path check observable.
+				_agent.SetDestination (Dest.Position);
+				pathCheckObs = _pathCheckObs;
+
+			} else {
+				// Disable obstacle and set path check observable that starts
+				// the path when possible.
+				_obstacle.enabled = false;
+				pathCheckObs = _delayedStrartPathCheckObs;
+			}
+
+			if (_pathCheckStream == null) {
+				// Subsribe to path check
+				_pathCheckStream = pathCheckObs
+					.Subscribe (reached => {
+						if (reached) {
+							Debug.Log (name + " Destination reached " + Dest);
+							this.Remove (Dest);
+						} else {
+							Debug.Log (name + " Destination removed.");
+						}
+						// Enable the obstable again and clear the path check
+						// stream.
+						_agent.enabled = false;
+						_obstacle.enabled = true;
+						_pathCheckStream.Dispose();
+						_pathCheckStream = null;
+					});
+			}
+
+			_agent.stoppingDistance = Dest.Distance;
+
+			return pathCheckObs;
 		}
 
 		#endregion

fc3b5f98eb8eef6a2da32843397e3dd401f90c63
Carlos Ginés <efrel.v2@gmail.com>
2016-09-16 21:33:03 +0200

    Added Shadow toggle

--- Assets/Scripts/Gameplay/UICtrl.cs
+++ Assets/Scripts/Gameplay/UICtrl.cs
@@ -5,9 +5,21 @@ using System.Collections;
 namespace EfrelGames {
 	public class UICtrl : MonoBehaviour {
 
+		public Light mainLight;
+
 		public void RestartScene ()
 		{
 			SceneManager.LoadScene (SceneManager.GetSceneAt (0).name);
+		}
+
+		public void ToggleShadows ()
+		{
+			if (mainLight.shadows == LightShadows.None) {
+				mainLight.shadows = LightShadows.Hard;
+			} else {
+				print ("Lights off");
+				mainLight.shadows = LightShadows.None;
+			}
 		}
 	}
 }
